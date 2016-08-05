45a6a37ced027df458b17aa01dfed29cb8d788e7
Sam Gruse <sgruse89@gmail.com>
2016-08-05 14:27:11 -0700

    Fixed the require.ensure issue with webpack-extract-text

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+require('./styles.scss');
 import $ from 'jquery';
 import Button from './Components/Button';
 
@@ -8,11 +9,31 @@ button.render('a');
 // Everything in the ensure callback will be split into a chunk, a seperate bundle that webpack will only load if needed.
 // In this case it will be loaded if there is an 'a' tag to attach to.
 
-if (document.querySelectorAll('a').length) {
-    require.ensure([], () => {
-        const Button = require('./Components/Button').default;
-        const button = new Button('google.com');
+// if (document.querySelectorAll('a').length) {
+//     require.ensure([], () => {
+//         const Button = require('./Components/Button').default;
+//         const button = new Button('google.com');
+//
+//         button.render('a');
+//     }, 'button');
+// }
 
-        button.render('a');
-    });
-}
+// If we have an anchor, render the Button component on it
+
+// if (document.querySelectorAll('a').length) {
+//     require.ensure([], () => {
+//         const Button = require('./Components/Button');
+//         const button = new Button('google.com');
+//
+//         button.render('a');
+//     });
+// }
+//
+// // If we have a title, render the Header component on it
+// if (document.querySelectorAll('h1').length) {
+//     require.ensure([], () => {
+//         const Header = require('./Components/Header');
+//
+//         new Header().render('h1');
+//     }, 'header');
+// }
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,89 @@
 'use strict';
 
+var webpack = require('webpack');
+var CleanPlugin = require('clean-webpack-plugin');
+var ExtractPlugin = require('extract-text-webpack-plugin');
+var production = process.env.NODE_ENV === 'production';
+
+var plugins = [
+    new ExtractPlugin('bundle.css'),
+    new webpack.DefinePlugin({
+      __SERVER__:      !production,
+      __DEVELOPMENT__: !production,
+      __DEVTOOLS__:    !production,
+      'process.env':   {
+        BABEL_ENV: JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+        name:      'main', // Move dependencies to our main file
+        children:  true, // Look for common dependencies in all children,
+        minChunks: 2 // How many times a dependency must come up before being extracted
+    }),
+  ];
+
+if (production) {
+  plugins = plugins.concat([
+    new CleanPlugin('builds'),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin()
+    // new webpack.optimize.MinChunkSizePlugin({
+    //   minChunkSize: 51200, // ~50kb
+    // }),
+    // new webpack.optimize.UglifyJsPlugin({
+    //   mangle:   true,
+    //   compress: {
+    //     warnings: false, // Suppress uglification warnings
+    //   },
+    // }),
+    // new webpack.DefinePlugin({
+    //   __SERVER__:      !production,
+    //   __DEVELOPMENT__: !production,
+    //   __DEVTOOLS__:    !production,
+    //   'process.env':   {
+    //     BABEL_ENV: JSON.stringify(process.env.NODE_ENV),
+    //   },
+    // }),
+  ])
+}
+
 module.exports = {
+  debug:   !production,
+  devtool: production ? false : 'eval',
   entry: `${__dirname}/src/index`,
   output: {
     path: 'builds',
-    filename: 'bundle.js',
-    publicPath: 'builds/',
+    filename: production ? '[name]-[hash].js' : 'bundle.js',
+    chunkFilename: '[name]-[chunkhash].js',
+    publicPath: 'builds/'
   },
+  plugins: plugins,
+
   module: {
     loaders: [
+      // zoolander
+      {
+          test: /\.scss$/,
+          loader: ExtractPlugin.extract('style', 'css!sass', { allChunks: true })
+      },
       {
           test:    /\.js$/,
           loader:  'babel',
-          include: __dirname + '/src',
+          include: __dirname + '/src'
       },
-      {
-          test:   /\.scss$/,
-          loader: 'style!css!sass',
-          // Or
-          // loaders: ['style', 'css', 'sass'],
-      },
+      // {
+      //     test:   /\.scss$/,
+      //     loader: 'style!css!sass',
+      //     // loader: ExtractPlugin.extract('style', 'css!sass'),
+      //     // Or
+      //     // loaders: ['style', 'css', 'sass'],
+      // },
       {
           test:   /\.html$/,
-          loader: 'html',
+          loader: 'html'
       }
-    ],
-  },
+    ]
+  }
   // stats: {
   //   reasons: true,
   //   errorDetails: true
